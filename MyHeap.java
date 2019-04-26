014197fb78049937718f2243655eab5e8f419062
rleongPd5 <rleong00@stuy.edu>
2019-04-26 15:41:22 -0400

    attempting a recursive pushdown

--- MyHeap.java
+++ MyHeap.java
@@ -14,6 +14,7 @@ public class MyHeap{
     return height;
   }
   private static void pushDown(int[]data,int size,int index){
+    /*
     //finding bound of the tree
     int s = size;
     //int height = 0;
@@ -29,10 +30,36 @@ public class MyHeap{
 
     //}
     //while(index<size
+    */
+
+    //storing indices
+    int left = (2*index) + 1;
+    int right = (2*index) + 2;
+    //check if parent is a leaf
+    if (left >= size) return;
+    //case 1: left greater than parent
+    //System.out.println(left);
+    //System.out.println(size);
+    if (data[left] > data[index]){
+      int temp = data[left];
+      data[left] = data[index];
+      data[index] = temp;
+      pushDown(data, size, left);
+    }
+    //case 2: right is nonexistent
+    else if (right >= size) return;
+    //case 3: right is greater than parent
+    else if (data[right]>data[index]){
+      int temp = data[right];
+      data[right] = data[index];
+      data[index] = temp;
+      pushDown(data, size, right);
+    }
+    //case 4: none are greater than parent
   }
   /*
     - size  is the number of elements in the data array.
-    - push the element at index i downward into the correct position. This will swap with the larger of the child nodes provided thatchild is larger. This stops when a leaf is reached, or neither child is larger. [ should be O(logn) ]
+    - push the element at index i downward into the correct position. This will swap with the larger of the child nodes provided that child is larger. This stops when a leaf is reached, or neither child is larger. [ should be O(logn) ]
     - precondition: index is between 0 and size-1 inclusive
     - precondition: size is between 0 and data.length-1 inclusive.
   */
@@ -67,7 +94,7 @@ public class MyHeap{
   //- sort the array by converting it into a heap then removing the largest value n-1 times. [ should be O(nlogn) ]
   public static void main(String[] args){
     int[] indices = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-    int[] a1 = {24, 9, 4, 8, 17, 6, 1, 6, 3, 19, 0, 1, 7, -9};
+    int[] a1 = {24, 9, 4, 8, 17, 6, 1, 6, 3, 19, 0, 1, 7, 39};
     //          0   1  2  3  4   5  6  7  8  9  10 11 12  13
     //length 14
     System.out.println("Indices tree: ");
@@ -75,15 +102,29 @@ public class MyHeap{
     System.out.println("\na1:");
     System.out.println(HeapHelp.toString(a1));
     int[] pushUpTests = {5, 0, 10, 4, 9};
+    System.out.println("\n\n-------Testing pushUp-------");
     for (int i = 0; i < pushUpTests.length; i++){
       System.out.println("Testing pushUp with index "+pushUpTests[i] +": ");
       pushUp(a1, pushUpTests[i]);
       System.out.println(HeapHelp.toString(a1));
     }
-    System.out.println("Testing height(size)");
+    // edited a1
+    //24, 19, 6, 8, 17, 4, 1, 6, 3, 9, 0, 1, 7, 39
+    //0    1  2  3  4   5  6  7  8  9  10 11 12 13
+
+
+    System.out.println("\n\n-------Testing height(size)-------");
     System.out.println(height(a1.length));
-    System.out.println("Testing pushDown");
-    pushDown(a1, 14, 5);
+
+
+    System.out.println("\n\n-------Testing pushDown-------");
+    int[] pushDownTests = {5, 1, 3, 13};
+    for (int i = 0; i < pushDownTests.length; i++){
+      System.out.println("Testing pushDown with index "+pushUpTests[i] +": ");
+      pushDown(a1, pushDownTests.length, pushDownTests[i]);
+      System.out.println(HeapHelp.toString(a1));
+    }
+    System.out.println(HeapHelp.toString(a1));
   }
 }
 
